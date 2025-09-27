75764a46290f35611c26ff46183bd397691276ad
vanillaisherez <vanillaisherez@gmail.com>
2025-09-26 22:22:11 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/f74e95a373ab49ea8013a2c3aee0ae22"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/41c39ddc61fa421fb9f709dab7acd906"))()
