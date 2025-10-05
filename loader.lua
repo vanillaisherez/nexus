f1bf1aa761c2786578913d611a1ad312bd6b2ae1
vanillaisherez <vanillaisherez@gmail.com>
2025-10-05 17:43:31 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/1f83dbad88d346e9a18ae4eb401f4637"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/449378250b9cb5ca"))()
